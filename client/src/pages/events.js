3dbc10f3cc32db23f8254af1ead775094d8e2251
hank amadeus <hankamadeus@email.com>
2020-08-20 20:20:53 -0400

    created event page and eventsearch component

---
+++ client/src/pages/events.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import EventCarousel from "../components/EventSearch/index";
+
+export default class Events extends Component {
+  render() {
+    return <EventCarousel />;
+  }
+}
