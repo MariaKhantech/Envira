ca2db027f36b6b1b6d223b91f401421b649bc54f
Gus Heptig <gusheptig@gmail.com>
2020-08-27 14:57:39 -0400

    tyring to get everyone on smae page

--- client/src/pages/Explore.js
+++ client/src/pages/Explore.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Container from "react-bootstrap/Container";
 import ExploreWrapper from "../components/ExploreWrapper/index";
 
+// Change to get explore page on maria's branch
 export default class Explore extends Component {
   render() {
     return (
