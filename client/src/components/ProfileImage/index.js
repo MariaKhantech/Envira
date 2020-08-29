a842c0874a22ee0acbc3f32de6d5ab1bfd07a0a6
Pri_030 <priyankasingh30@gmail.com>
2020-08-29 17:55:15 -0400

    installed react-avatar-edit package

--- client/src/components/ProfileImage/index.js
+++ client/src/components/ProfileImage/index.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import Avatar from 'react-avatar-edit'
 
 export default class index extends Component {
+
+    state = {
+        preview: null,
+        src: "https://via.placeholder.com/150",
+
+
+    }
+
+
+    onClose = () => {
+        this.setState({ preview: null })
+    }
+
+    onCrop = (preview) => {
+        this.setState({ preview })
+    }
     render() {
         return (
             <div>
-                <p>hello</p>
+
+                <Avatar
+                    width={390}
+                    height={295}
+                    onCrop={this.onCrop}
+                    onClose={this.onClose}
+                    src={this.state.src}
+                />
+                <img src={this.state.preview} alt="Preview" />
+
             </div>
         )
     }
