2e640ffb7145b81da9be16f14f9fa7dedeb3612c
Pri_030 <priyankasingh30@gmail.com>
2020-08-30 00:05:00 -0400

    updated profileimage component

--- client/src/components/ProfileImage/index.js
+++ client/src/components/ProfileImage/index.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react'
 
 export default class index extends Component {
-    state={
+    state = {
         file: '',
         imagePreviewUrl: ''
     }
 
+    handleImageChange = (event) => {
+        event.preventDefault();
 
+        let reader = new FileReader();
+        let file = event.target.files[0];
+        console.log(reader)
+
+        reader.onloadend = () => {
+            this.setState({
+                file: file,
+                imagePreviewUrl: reader.result
+            });
+        }
+
+        reader.readAsDataURL(file)
+    }
 
     render() {
+        let { imagePreviewUrl } = this.state;
+        let $imagePreview = null;
+        if (imagePreviewUrl) {
+            $imagePreview = (<img src={imagePreviewUrl} />);
+        } else {
+            $imagePreview = (<div className="previewText">Please select an Image for Preview</div>);
+        }
         return (
             <div>
                 <div className="imgPreview">
