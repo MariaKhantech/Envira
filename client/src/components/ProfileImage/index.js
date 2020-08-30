94b2436ca836744c155fb22e7b3fc675659b4060
Pri_030 <priyankasingh30@gmail.com>
2020-08-30 00:01:26 -0400

    updated profileimage component

--- client/src/components/ProfileImage/index.js
+++ client/src/components/ProfileImage/index.js
@@ -4,7 +4,17 @@ export default class index extends Component {
     render() {
         return (
             <div>
-                <h1>Hello</h1>
+                <div className="imgPreview">
+                    {$imagePreview}
+                </div>
+                <form onSubmit={(e) => this._handleSubmit(e)}>
+                    <input className="fileInput"
+                        type="file"
+                        onChange={(e) => this._handleImageChange(e)} />
+                    <button className="submitButton"
+                        type="submit"
+                        onClick={(e) => this._handleSubmit(e)}>Upload Image</button>
+                </form>
             </div>
         )
     }
