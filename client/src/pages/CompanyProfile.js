23d0c5f1d59c60cba03a26a26efb551a6d762060
Pri_030 <priyankasingh30@gmail.com>
2020-09-02 13:08:52 -0400

    updated CompanyProfile page

---
+++ client/src/pages/CompanyProfile.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import CompanyProfile from "../components/CompanyProfile";
+
+
+export default class CompanyProfile extends Component {
+    render() {
+
+        return (
+            <div>
+                <CompanyProfile ></CompanyProfile>
+            </div>
+        )
+    }
+}
