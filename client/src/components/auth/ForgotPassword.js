32e84f68ba55c350029ae8c2765ecfb75410a6b4
mmkhan <mariatechfuture@gmail.com>
2020-09-08 22:32:48 -0400

    styled forgot password model

--- client/src/components/auth/ForgotPassword.js
+++ client/src/components/auth/ForgotPassword.js
@@ -59,17 +59,17 @@ export default class ForgotPassword extends Component {
       <>
         <div className="container mt-5">
           <div className="row justify-content-center align-items-center">
-            <div className="col-md-7 border mt-2 shadow-lg p-3 mb-5 bg-white rounded">
+            <div className="col-md-7  mt-2 shadow-lg p-3 mb-5 bg-white border-style-signup rounded">
               <h2 className="font-weight-normal text-center">Forgot your password?</h2>
-              <p className="font-italic">Not to worry. Just enter your email address below and we'll send you an instruction email for recovery.</p>
+              <p className="font-italic text-center">Not to worry. Just enter your email address below and we'll send you an instruction email for recovery.</p>
               <form onSubmit={this.handleForgotPassword} className="mt-3">
                 <div className="form-group">
                   <input name="email" value={this.state.email}
-                    onChange={this.handleInputChange} className="form-control form-control-lg" type="email" placeholder="Your email address" />
+                    onChange={this.handleInputChange} className="form-control form-control-lg" type="email" placeholder="Email address" />
                 </div>
                 <div style={myStyle}>{this.state.errors.email}</div>
                 <div className="form-group mx-auto text-center">
-                  <button type="submit" className="btn btn-lg btn-success">Reset Password</button>
+                  <button type="submit" className="btn btn-lg create-btn font-weight-bold">Reset Password</button>
                 </div>
               </form>
             </div>
