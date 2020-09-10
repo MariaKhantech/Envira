4afd24a901fa939a07d7be7ea7e878e5abdfd029
Gus Heptig <gusheptig@gmail.com>
2020-09-09 22:44:08 -0400

    added images to carousel, fixed link between search and eventpage, and how data is rendered

--- client/src/components/EventCard/index.js
+++ client/src/components/EventCard/index.js
@@ -71,8 +71,12 @@ export class index extends Component {
   }
 
   getEventData = () => {
-    const id = JSON.parse(localStorage.getItem("eventId")).id;
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const id = urlParams.get("eventId");
+
     console.log(id);
+
     this.setState({ eventId: id });
 
     Axios.get(`/api/create/eventcreate`)
@@ -135,8 +139,9 @@ export class index extends Component {
   //   //   Post request to submit rating to DB
   postRating = (event) => {
     event.preventDefault();
-    const id = JSON.parse(localStorage.getItem("eventId")).id;
-    console.log(id);
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const id = urlParams.get("eventId");
     Axios.post(`/api/rate/event`, {
       rating: this.state.rating,
       EventId: id,
